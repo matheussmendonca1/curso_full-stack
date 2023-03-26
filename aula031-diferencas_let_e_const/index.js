b3931cde198eb20b373eab2081dbbc7ca0f37f68
Matheus Mendonça <m.e.mendonca2007@gmail.com>
2023-03-26 10:05:53 -0300

    mais diferenças entre let, const e var.

---
+++ aula031-diferencas_let_e_const/index.js
@@ -0,0 +1,39 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco
+// Var tem escopo de função
+
+// let nome = 'Luiz'; // criando
+// var nome2 = 'Luiz';
+
+
+
+// if(verdadeira) {
+//     let nome = 'Otávio'; // criando
+//     var nome2 = 'Rogério';
+    
+//     if(verdadeira) {
+//         var nome2 = 'Ronaldo'; // Redeclarada
+//         let nome = 'Otra coisa';
+//     }
+// }
+
+// console.log(nome, nome2);
+
+// function falaOi() {
+//     var sobrenome = 'Miranda';
+
+//     if(verdadeira) {
+//         let nome = 'Luiz';
+//        console.log(sobrenome); 
+//     }
+// }
+
+// console.log(nome);
+
+// falaOi();
+
+console.log(sobrenome);
+
+
+let sobrenome = 'Miranda';
