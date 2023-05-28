a1ceca44baad9f639f416a5a5b36bbf873b4d0fb
Matheus Mendonça <m.e.mendonca2007@gmail.com>
2023-05-28 17:35:43 -0300

    calculadora com constructor function em JS

--- aula058-ex_constructor_functions/assets/js/main.js
+++ aula058-ex_constructor_functions/assets/js/main.js
@@ -1,63 +1,53 @@
 function Calculadora() {
-    display: document.querySelector('.display');
-    btnClear: document.querySelector('.btn-clear');
-
-    this.inicia = function() {
-        cliqueBotoes();
+    this.display = document.querySelector('.display');
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
     };
 
-    this.realizaConta = function() {
-        let conta = this.display.value;
-        try {
-            conta = eval(conta);
-
-            if(!conta) {
-                alert('Conta inválida!');
-            }
-
-            this.display.value = String(conta);
-        } catch(e) { // Caso os dígitos do display sejam inválidos
-            alert('Conta inválida!');
-            return;
-        }
-    };
-
-    this.clearDisplay = function() {
-        display.value = ''; // Para limpar o display após o cálculo
-    };
-
-    this.apagaUm = function() {
-        this.display.value = this.display.value.slice(0, -1); // Para remover um dígito por vez quando 'delete' for clicado.
-    };
-
-    this.cliqueBotoes = function() {
-        // this -> calculadora
-        document.addEventListener('click', e => {
-            const el = e.target;
-
-            if(el.classList.contains('btn-num')) {
-                this.btnParaDisplay(el.innerText);
-            }
-
-            if(el.classList.contains('btn-clear')) {
-                this.clearDisplay();
-            }
-            
-            if(el.classList.contains('btn-del')) {
-                this.apagaUm();
-            }
-
-            if(el.classList.contains('btn-eq')) {
+    this.capturaEnter = () => {
+        document.addEventListener('keypress', (e) => {
+            if(e.keyCode === 13) {
                 this.realizaConta();
             }
-        }); // arrow function faz com que o 'this' seja referente a calculadora e não ao document
+        });
+    };
+    
+    this.capturaCliques = () => {
+        document.addEventListener('click', (event) => {
+            const el = event.target;
+            if(el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if(el.classList.contains('btn-clear')) this.clear(el);
+            if(el.classList.contains('btn-del')) this.del(el);
+            if(el.classList.contains('btn-eq')) this.realizaConta(el);
+        });
     };
 
-    this.btnParaDisplay = function(valor) {
-        this.display.value += valor;
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
+
+            if(!conta) {
+                alert('Conta inválida');
+                return;
+            }
+
+            this.display.value = conta;
+        } catch(e) {
+            alert('Conta inválida');
+            return;
+        }
+    }
+
+    this.addNumDisplay = (el) => {
+        this.display.value += el.innerText;
+        this.display.focus();
     };
 
+    this.clear = () => this.display.value = '';
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
 }
 
 const calculadora = new Calculadora();
+
 calculadora.inicia();
