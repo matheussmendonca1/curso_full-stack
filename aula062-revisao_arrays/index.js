c7c6794753a7dd2a29c2d87fdbf4d2e550f9af6c
Matheus Mendonça <m.e.mendonca2007@gmail.com>
2023-06-17 11:11:35 -0300

    mais alguns tipos de funções e revisão de arrays

---
+++ aula062-revisao_arrays/index.js
@@ -0,0 +1,22 @@
+// const nomes = new Array('Eduardo', 'Maria', 'Joana');
+// nomes[2] = 'João';
+// delete nomes[2];
+// const novo = [...nomes];
+
+// novo.pop();
+// console.log(nomes);
+// console.log(novo);
+// const removido = nomes.shift();
+
+// nomes.unshift('João');
+// nomes.unshift('Wallace');
+
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// const novo = nomes.slice(1, -1);
+// console.log(novo);
+// const nomes = nome.split(' ');
+
+const nomes = ['Luiz', 'Otávio',  'Miranda'];
+const nome = nomes.join(' ');
+console.log(nomes);
+console.log(nome);
