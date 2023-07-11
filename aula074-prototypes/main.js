3981dee36260cf3faf79b1e613aad3795aa8dc75
Matheus Mendonça <m.e.mendonca2007@gmail.com>
2023-07-11 12:28:46 -0300

    prototypes de objetos + manipulação de prototypes

---
+++ aula074-prototypes/main.js
@@ -0,0 +1,45 @@
+/*
+Javascript é baseado em protótipos para passar propriedades e métodos
+de um objeto para outro.
+
+Definição de protótipo
+Protótipo é o termo usado para se referir ao que foi criado pela
+primeira vez, servindo de modelo ou molde para futuras produções.
+
+Todos os objetos tem uma referência interna para um protótipo (__proto__)
+que vem da propriedade prototype da função construtora que foi usada para
+criá-lo. Quando tentamos acessar um membro de um objeto, primeiro o motor
+do JS vai tentar encontrar este membro no próprio objeto e depois a cadeia
+de protótipos é usada até o topo (null) até encontrar (ou não) tal membro.
+*/
+
+// function Pessoa(nome, sobrenome) {
+//     this.nome = nome;
+//     this.sobrenome = sobrenome;
+//     // this.nomeCompleto = () => 'ORIGINAL: ' + this.nome + ' ' + this.sobrenome;
+//   }
+  
+//   // Pessoa.prototype === pessoa1.__proto__
+  
+//   Pessoa.prototype.nomeCompleto = function() {
+//     return this.nome + ' ' + this.sobrenome;
+//   };
+  
+//   const pessoa1 = new Pessoa('Luiz', 'O.'); // <- Pessoa = Função construtora
+//   const pessoa2 = new Pessoa('Maria', 'A.'); // <- Pessoa = Função construtora
+//   const data = new Date(); // <-- Date = Função construtora
+  
+//   console.dir(pessoa1);
+//   console.dir(data);
+
+/* TESTE
+function Pessoa() {}
+
+Pessoa.prototype.nomeCompleto = (nome, sobrenome) => {
+    return nome + ' ' + sobrenome;
+};
+
+const pessoa1 = new Pessoa();
+const pessoa2 = pessoa1.nomeCompleto('Matheus', 'Mendonça');
+console.log(pessoa2); 
+*/
