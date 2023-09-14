4efc3246d2d7a87f80fc6c1823a0699f91fdbe6e
Matheus Mendonça <m.e.mendonca2007@gmail.com>
2023-09-13 21:09:03 -0300

    aulas de HttpRequest e fetch API

---
+++ aula090-fecth_api/assets/js/main.js
@@ -0,0 +1,49 @@
+fetch('pagina04.html')
+.then(resposta => {
+    if(resposta.status !== 200) throw new Error('ERRO 404 NOSSO');
+    return resposta.text();
+})
+.then(html => console.log(html))
+.catch(e => console.error(e));
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if(tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+    try {
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+
+        if(response.status !== 200) throw new Error('ERRO 404!');
+
+        const html = await response.text();
+        carregaResultado(html);
+    } catch(e) {
+        console.log(e);
+    }
+    
+
+    // fetch(href)
+    //     .then(response => {
+    //         return response.text();
+    //     })
+    //     .then(html => carregaResultado(html))
+    //     .catch(e => console.log(e));
+
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+
+    resultado.innerHTML = response;
+}
+
+
+
